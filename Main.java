97fc68df64a89466829332998cea40fad8142cb0
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-03-22 11:45:19 +0000

    final commit

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int x;
+    double y;
+    x=24;
+    y=(x%10);    
+    System.out.println("Reszta z dzielenia to: "+y);
   }
 }
